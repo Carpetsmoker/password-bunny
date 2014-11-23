fde8caf8d0ddc066e9b70e367fcda7e3544e1877
Konstantin Schukraft <konstantin@schukraft.org>
2014-11-23 21:26:56 +0100

    Don't make path to parsejson.vim hardcoded; add lookup in "lib"-directory relative to curworkdir.

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 
 install -U pwbunny $HOME/local/bin/pwbunny
 install -U -m 644 pwbunny.vim $HOME/local/lib/pwbunny/pwbunny.vim
+install -U -m 644 parsejson.vim $HOME/local/lib/pwbunny/parsejson.vim
